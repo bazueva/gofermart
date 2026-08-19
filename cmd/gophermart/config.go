8c909904c18c189b1adb5545fac02890431b5672
bazueva <bazueva1988@gmail.com>
2026-08-19 22:07:41 +0500

    #1 Создать таблицу с пользователями

---
+++ cmd/gophermart/config.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	configpkg "github.com/bazueva/gofermart/cmd/config"
+	"github.com/caarlos0/env/v11"
+
+	"go.uber.org/zap"
+)
+
+type config struct {
+	ServerAddr  configpkg.ServerAddr `env:"RUN_ADDRESS"`
+	DatabaseDSN string               `env:"DATABASE_URI"`
+
+	logger *zap.Logger
+}
+
+func readConfig() (config, error) {
+	cfg := config{
+		ServerAddr: configpkg.ServerAddr{
+			Host: "localhost",
+			Port: 8080,
+		},
+	}
+
+	err := parseFlags(&cfg)
+	if err != nil {
+		return config{}, err
+	}
+
+	err = env.Parse(&cfg)
+	if err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func parseFlags(config *config) error {
+	serverFlags := flag.NewFlagSet("", flag.ContinueOnError)
+	serverFlags.Var(&config.ServerAddr, "a", "address http server")
+	serverFlags.StringVar(&config.DatabaseDSN, "d", "", "Database DSN")
+
+	if len(os.Args) > 1 {
+		err := serverFlags.Parse(os.Args[1:])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
